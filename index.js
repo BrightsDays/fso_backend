5e3e028dfeec94bcb562304979cb2b694323f040
vivanov <brightsdayss@gmail.com>
2022-07-01 18:28:57 +0400

    uibuild

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const errorHandler = (error, request, response, next) => {
         return response.status(400).send({ error: 'malformatted id'})
     }
     if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
+        return response.status(400).json({ error: error })
     }
 
     next(error)
